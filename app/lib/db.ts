736228b6d40e3e723aae3684eb58cd3e95864679
Ismail-anis-cherrak <ia.cherrak@esi-sba.dz>
2025-05-25 19:24:39 +0100

    added pdf

--- app/lib/db.ts
+++ app/lib/db.ts
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 
 const MONGODB_URI = process.env.DB_LINK as string;
-
+//coomment 
 export const connectDB = async () => {
   if (mongoose.connection.readyState >= 1) {
     console.log("Already connected to MongoDB");
